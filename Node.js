e6d74ea57341544b6fa137bc90c4356b41d7a293
1Majora1 <astroisbetter9@gmail.com>
2024-11-27 01:13:46 -0500

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,7 @@
+/proxy-website
+  /api
+    proxy.js        # This will be the proxy serverless function
+  index.html         # Simple front-end for the proxy
+  package.json
+  README.md
+  .gitignore
